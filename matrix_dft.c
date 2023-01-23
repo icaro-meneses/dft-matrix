7f93524628681a25dd8af84af60203eeecb3cb12
icaro-meneses <icaro.santana.dev@hotmail.com>
2023-01-22 21:07:14 -0300

    DFT Testing: matrix_dft.h and matrix_dft.c files creation

---
+++ matrix_dft.c
@@ -0,0 +1,22 @@
+/*
+ * ======================================================================
+ *
+ *       Filename:  matrix_dft.c
+ *
+ *    Description:  Source file for matrix_dft.h, including function
+ *					implementations.
+ *
+ *        Version:  1.0
+ *        Created:  22/01/23 21:04:37
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Icaro Meneses Ferreira de Santana
+ *					(Icaro Meneses),
+ *					icaro.santana.dev@hotmail.com
+ *
+ * ======================================================================
+ */
+
+#include "matrix_dft.h"
+#include "matrix_operations.h"
---
+++ matrix_dft.h
@@ -0,0 +1,26 @@
+/*
+ * ======================================================================
+ *
+ *       Filename:  matrix_dft.h
+ *
+ *    Description:  Header file for DFT(Matrix) implementation, which
+ *					contains function prototypes.
+ *
+ *        Version:  1.0
+ *        Created:  22/01/23 21:03:32
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Icaro Meneses Ferreira de Santana
+ *					(Icaro Meneses),
+ *					icaro.santana.dev@hotmail.com
+ *
+ * ======================================================================
+ */
+
+#ifndef MATRIX_DFT_LIB_H
+#define MATRIX_DFT_LIB_H
+
+#include <complex.h>
+
+#endif
